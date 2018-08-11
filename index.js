c7b5351358f15c61e927efbeb0fdf0391906d4fe
Preston A Arnette <arnettepreston@gmail.com>
2018-08-11 02:54:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,18 +15,21 @@ function nowServing (line) {
     return 'There is nobody waiting to be served!'
   } else {
     
-    line.shift()
-    return `Currently serving ${line[0]}.`
+    return `Currently serving ${line.shift()}.`
   }
 }
 
+// takes the array and returns it as a string list "The line is currently: 1. Ada, 2. Grace"
 function currentLine (line) {
-  
-  var currentMessage = `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]}`
-  var emptyLineMessage = 'The line is currently empty.'
-  
-  while (line.length > 0) {
-    return currentMessage
+  if (line.length === 0) {
+    return "The line is currently empty."
   }
-  return emptyLineMessage
+  
+  var lineList = []
+  
+  for (var i = 0, l = line.length; i < l; i++) {
+    lineList.push(`${i + 1}. ${line[i]}`)
+  }
+  
+  return `The line is currently: ${lineList.join(', ')}`
 }
